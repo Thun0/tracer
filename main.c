772317f2047aabdf04bb08118a30d42d4f8aaaa4
Pawel Jankowski <pjankow6@gmail.com>
2016-08-05 12:51:30 +0200

    tp init

--- main.c
+++ main.c
@@ -19,6 +19,17 @@
 
 char filename[FILEPATH_LEN+1];
 FILE* fp;
+int child_pid;
+
+typedef struct 
+{
+	long addr;
+	long original_code;
+	char name[SYMBOL_NAME_LEN+1];
+} Tracepoint;
+
+Tracepoint* tracepoints;
+int tp_count;
 
 void parse_elf_file()
 {
@@ -47,26 +58,33 @@ void parse_elf_file()
 
 			fseek(fp, section_header.sh_offset, SEEK_SET);
 			int entries = section_header.sh_size / section_header.sh_entsize;
-			printf("Found symtab with %d entries\n", entries);
+			//printf("Found symtab with %d entries\n", entries);
+			tracepoints = malloc(entries*sizeof(Tracepoint));//won't be bigger
 			for(i = 0; i < entries; ++i)
 			{
 				Elf64_Sym symbol;
 				fread(&symbol, 1, sizeof(symbol), fp);
 				if(ELF64_ST_TYPE(symbol.st_info) == STT_FUNC)
 				{
-					printf("Found function at offset %lx", symbol.st_value);
+					//printf("Found function at offset %lx", symbol.st_value);
 					if(symbol.st_name != 0)
 					{
 						long pos = ftell(fp);
 						fseek(fp, strtab_header.sh_offset+symbol.st_name, SEEK_SET);
-						char symbol_name[SYMBOL_NAME_LEN+1];
-						fread(symbol_name, SYMBOL_NAME_LEN, sizeof(char), fp);
-						printf(" %s", symbol_name);
+						//char symbol_name[SYMBOL_NAME_LEN+1];
+						//fread(symbol_name, SYMBOL_NAME_LEN, sizeof(char), fp);
+						//printf(" %s", symbol_name);
+
+						tracepoints[tp_count].addr = symbol.st_value;
+						fread(tracepoints[tp_count].name, SYMBOL_NAME_LEN, sizeof(char), fp);
+
 						fseek(fp, pos, SEEK_SET);
+						tp_count++;
 					}
-					printf("\n");
+					//printf("\n");
 				}
 			}
+			return;
 		}
 	}
 }
@@ -75,6 +93,11 @@ void parse_elf_file()
 void prepare_tracepoints()
 {
 	parse_elf_file();
+	for(int i = 0; i < tp_count; ++i)
+	{
+		tracepoints[i].original_code = ptrace(PTRACE_PEEKTEXT, child_pid, tracepoints[i].addr, 0);
+		printf("%s: %lx\n", tracepoints[i].name, tracepoints[i].original_code);
+	}
 }
 
 void trace()
@@ -90,7 +113,7 @@ int main(int argc, char** argv)
 		return -1;
 	}
 	strncpy(filename, argv[1], FILEPATH_LEN);
-	int child_pid = fork();
+	child_pid = fork();
 	if(child_pid == 0)
 	{
 		//child process
@@ -106,6 +129,7 @@ int main(int argc, char** argv)
 		//sleep(2);
 		prepare_tracepoints();
 		trace();
+		free(tracepoints);
 	}
 	return 0;
 }
